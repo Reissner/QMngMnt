f1bbf91fb7f49a99a56887b30b68b7bdd9c3ef75
Ernst Reissner <ernst.reissner@simuline.eu>
2022-08-14 19:19:09 +0200

    extended instVScode.sh and exchanged options

--- src/main/resources/eu/simuline/qMngmnt/installation/instVScode.sh
+++ src/main/resources/eu/simuline/qMngmnt/installation/instVScode.sh
@@ -5,54 +5,57 @@
 # The name of the extension is always name of the provider dot name of the extension.
 # I feel better solved with java: domain.
 # to update the version just
-# code --install-extension --force ms-vscode.cpptools
+# code --force --install-extension ms-vscode.cpptools
 
 # Textual: markdown  
-code --install-extension --force DavidAnson.vscode-markdownlint
-code --install-extension --force joffreykern.markdown-toc
-code --install-extension --force yzhang.markdown-all-in-one
-code --install-extension --force shd101wyy.markdown-preview-enhanced
+code --force --install-extension DavidAnson.vscode-markdownlint
+code --force --install-extension joffreykern.markdown-toc
+code --force --install-extension yzhang.markdown-all-in-one
+code --force --install-extension shd101wyy.markdown-preview-enhanced
 #code --uninstall-extension --force jrieken.md-navigate
-code --install-extension --force alanwalk.markdown-navigation
-code --install-extension --force tchayen.markdown-links
+code --force --install-extension alanwalk.markdown-navigation
+code --force --install-extension tchayen.markdown-links
 
 # antlr
-code --install-extension --force mike-lischke.vscode-antlr4
-code --install-extension --force ncthbrt.antlr4
+code --force --install-extension mike-lischke.vscode-antlr4
+code --force --install-extension ncthbrt.antlr4
 
 # xml
-code --install-extension --force redhat.vscode-xml
-code --install-extension --force dotjoshjohnson.xml
+code --force --install-extension redhat.vscode-xml
+code --force --install-extension dotjoshjohnson.xml
 
 # latex
-code --install-extension --force james-yu.latex-workshop
-code --install-extension --force phr0s.bib
-code --install-extension --force twday.bibmanager
+code --force --install-extension james-yu.latex-workshop
+code --force --install-extension phr0s.bib
+code --force --install-extension twday.bibmanager
+code --force --install-extension mathematic.vscode-latex
 # TBD: zotero
 
 #git
-code --install-extension --force eamodio.gitlens
+code --force --install-extension eamodio.gitlens
 
 # related with maven (mostly java) 
 # vulnerabilities
-code --install-extension --force redhat.fabric8-analytics
+code --force --install-extension redhat.fabric8-analytics
 
 #
-#code --install-extension --force Gimly81.matlab
-#code --install-extension --force ms-python.python
-#code --install-extension --force bmewburn.vscode-intelephense-client # php
+#code --force --install-extension Gimly81.matlab
+#code --force --install-extension ms-python.python
+#code --force --install-extension bmewburn.vscode-intelephense-client # php
 #code --install-extension v--force scjava.vscode-java-pack# includes java5 runner
 
 # to start vs code on the current directory just type
 # code .
 # this will start in the background automatically.
 
+code --force --install-extension ithildir.java-properties
+
 # for docker 
-code --install-extension --force ms-azuretools.vscode-docker
-code --install-extension --force ms-vscode-remote.remote-containers
+code --force --install-extension ms-azuretools.vscode-docker
+code --force --install-extension ms-vscode-remote.remote-containers
 
 # spellchecker
-code --install-extension --force valentjn.vscode-ltex
+code --force --install-extension valentjn.vscode-ltex
 
 #code --list-extensions
 #code --install-extension ms-vscode.cpptools
@@ -60,3 +63,22 @@ code --install-extension --force valentjn.vscode-ltex
 #code --install-extension Gimly81.matlab
 #code --install-extension DavidAnson.vscode-markdownlint
 
+# groovy
+code --force --install-extension mellowmarshmallow.groovy
+code --force --install-extension nicolasvuillamy.vscode-groovy-lint
+
+# java
+code --force --install-extension redhat.java
+code --force --install-extension vscjava.vscode-java-test
+code --force --install-extension vscjava.vscode-java-dependency
+code --force --install-extension vscjava.vscode-java-debug
+code --force --install-extension vscjava.vscode-maven
+code --force --install-extension vscjava.vscode-java-pack
+code --force --install-extension tintinweb.vscode-decompiler
+code --force --install-extension dgileadi.java-decompiler
+
+
+
+
+# makefile 
+code --force --install-extension carlos-algms.make-task-provider
