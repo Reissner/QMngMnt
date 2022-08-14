3b85006ce90ea6bbf8516b2b91598bc1823ee43a
Ernst Reissner <ernst.reissner@simuline.eu>
2022-08-14 19:22:12 +0200

    Update addRepos.sh: gooble singed key only if necessary

--- src/main/resources/eu/simuline/qMngmnt/installation/addRepos.sh
+++ src/main/resources/eu/simuline/qMngmnt/installation/addRepos.sh
@@ -1,3 +1,5 @@
+wget -O find_uno.py https://gist.githubusercontent.com/regebro/036da022dc7d5241a0ee97efdf1458eb/raw/find_uno.py
+#python3 find_uno.py
 # for video; priority 90
 zypper ar -c 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ packman
 
@@ -6,7 +8,10 @@ zypper ar https://download.opensuse.org/repositories/home:plater/Tumbleweed/home
 
 # for google chrome 
 zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
-wget https://dl.google.com/linux/linux_signing_key.pub
+if ! test linux_signing_key.pub ;
+then
+  wget https://dl.google.com/linux/linux_signing_key.pub
+fi
 rpm --import linux_signing_key.pub
 
 
@@ -15,3 +20,5 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
 
 zypper refresh -f
+
+
