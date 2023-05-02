467f4aff9a5fd1e648c43a5964d7db3c917b3a09
Ernst Reissner <ernst.reissner@simuline.eu>
2023-05-02 13:16:18 +0200

    Update instGCexts.sh: more extensions.

--- src/main/resources/eu/simuline/qMngmnt/installation/instGCexts.sh
+++ src/main/resources/eu/simuline/qMngmnt/installation/instGCexts.sh
@@ -1,17 +1,35 @@
 #!/bin/sh -
 
 dir=/opt/google/chrome/extensions
-declare -A array
-array[jabref]=bifehkofibaamoeaopjglfkddgkijdlh
-array[markdown]=ckkdlimhmcjmikdlpkmbgfkaikojcbjk
-array[googleDocs]=ghbmnnjooekpmoecnnnilnnbdlolhkhi
-array[seAutoRefresh]=globgafddkdlnalejlkcpaefakkhkdoa
-array[vDownloader]=eooikgjpbiiaebbbnjbcnmgggekfnhfj
-array[webSecurity]=jdacdjpphmjekpfghmgfmniodedbdpih
-array[zoomScheduler]=kgjfgplpablkjnlkjmjdecgdpfankdle
+#dir=/usr/share/google-chrome/extensions/
+declare -A exts
+# jabref browser extension 
+exts[jabref]=bifehkofibaamoeaopjglfkddgkijdlh
+# BibItNow 0.908
+exts[BibItNow]=bmnfikjlonhkoojjfddnlbinkkapmldg
 
-for i in "${!array[@]}"
+# https://github.com/simov/markdown-viewer
+exts[markdown]=ckkdlimhmcjmikdlpkmbgfkaikojcbjk
+
+exts[googleDocs]=ghbmnnjooekpmoecnnnilnnbdlolhkhi
+
+exts[seAutoRefresh]=globgafddkdlnalejlkcpaefakkhkdoa
+# Auto Refresh Plus | Page Monitor 7.5.0
+exts[seAutoRefreshB]=hgeljhfekpckiiplhkigfehkdpldcggm
+
+# VideoDowloader 2.5 videosaverpro.com
+exts[vDownloaderB]=ihackilhcefdlfoehlhlamloflhofcme
+exts[vDownloader]=eooikgjpbiiaebbbnjbcnmgggekfnhfj
+
+# AVG online security www.avg.com
+exts[avgSecurity]=nbmoafcmbajniiapeidgficgifbfmjfo
+exts[webSecurity]=jdacdjpphmjekpfghmgfmniodedbdpih
+
+# zoom scheduler 1.8.16 
+exts[zoomScheduler]=kgjfgplpablkjnlkjmjdecgdpfankdle
+
+for i in "${!exts[@]}"
 do
-  echo "install extension  : $i with id: ${array[$i]}"
-cp ./chromeExtId.json $dir/${array[$i]}
+  echo "install extension  : $i with id: ${exts[$i]}"
+  cp ./chromeExtId.json $dir/${exts[$i]}
 done
