1128f7b7ae40f61b154fe2debf863f4fdb27d5bd
Ernst Reissner <ernst.reissner@simuline.eu>
2024-02-28 10:11:36 +0100

    Update instNPM.sh: added packages to develop extensions for VS code.

--- src/main/resources/eu/simuline/qMngmnt/installation/instNPM.sh
+++ src/main/resources/eu/simuline/qMngmnt/installation/instNPM.sh
@@ -4,4 +4,8 @@
 
 npm update -g npm
 npm update -g tar
+# to create an a frontend (a bit outdated)
 npm update -g create-react-app
+# for development of VS code extensions 
+npm install -g yo generator-code
+npm install -g vsce
